8d87da92419df588ea52f6b5dde90da56acccf5f
AliA1997-Medium <devmtnali@gmail.com>
2018-07-22 23:43:46 -0700

    Completed SOcial Media Links

--- src/components/userSubComponents/SocialMediaSection/SocialMediaSection.js
+++ src/components/userSubComponents/SocialMediaSection/SocialMediaSection.js
@@ -11,6 +11,9 @@ import './SocialMediaSection.css';
 import { connect } from 'react-redux';
 
 class SocialMediaSection extends Component {
+    clickLink = (link) => {
+        if(window.confirm('You sure you want to go to third party site?')) window.open(link, '_blank');
+    }
     render() {
         const { currentUser } = this.props;
         console.log('currentUser Social media---------------', currentUser)
@@ -18,14 +21,22 @@ class SocialMediaSection extends Component {
             <div>
                 Social Media Section
                 {currentUser.social_media && currentUser.social_media.length ? currentUser.social_media.map((item, i) => {
-                    if(item.includes('twitter')) return <IoSocialTwitter key={i} className='dashboard-social-icons'/>
-                    else if(item.includes('facebook')) return <IoSocialFacebook key={i} className='dashboard-social-icons'/>
-                    else if(item.includes('instagram')) return <IoSocialInstagram key={i}className='dashboard-social-icons'/>
-                    else if(item.includes('snapchat')) return <IoSocialSnapchat key={i} className='dashboard-social-icons'/>
-                    else if(item.includes('twitch')) return <IoSocialTwitch key={i} className='dashboard-social-icons'/>
-                    else if(item.includes('reddit')) return <IoSocialReddit key={i} className='dashboard-social-icons'/>
-                    else if(item.includes('playstation')) return <IoPlaystation key={i} className='dashboard-social-icons'/>
-                    else if(item.includes('xbox')) return <IoXbox className='dashboard-social-icons'/>
+                    if(item.includes('twitter')) return <IoSocialTwitter key={i} className='dashboard-social-icons twitter-icon' 
+                                                        onClick={() => this.clickLink(item)}/>
+                    else if(item.includes('facebook')) return <IoSocialFacebook key={i} className='dashboard-social-icons facebook-icon' 
+                                                        onClick={() => this.clickLink(item)}/>
+                    else if(item.includes('instagram')) return <IoSocialInstagram key={i}className='dashboard-social-icons instagram-icon' 
+                                                        onClick={() => this.clickLink(item)}/>
+                    else if(item.includes('snapchat')) return <IoSocialSnapchat key={i} className='dashboard-social-icons snapchat-icon' 
+                                                        onClick={() => this.clickLink(item)}/>
+                    else if(item.includes('twitch')) return <IoSocialTwitch key={i} className='dashboard-social-icons twitchtv-icon' 
+                                                        onClick={() => this.clickLink(item)}/>
+                    else if(item.includes('reddit')) return <IoSocialReddit key={i} className='dashboard-social-icons reddit-icon' 
+                                                        onClick={() => this.clickLink(item)}/>
+                    else if(item.includes('playstation')) return <IoPlaystation key={i} className='dashboard-social-icons playstation-icon' 
+                                                        onClick={() => this.clickLink(item)}/>
+                    else if(item.includes('xbox')) return <IoXbox className='dashboard-social-icons xbox-icon' 
+                                                        onClick={() => this.clickLink(item)}/>
                     else return <p></p>
                 }) : <p></p>}
             </div>
